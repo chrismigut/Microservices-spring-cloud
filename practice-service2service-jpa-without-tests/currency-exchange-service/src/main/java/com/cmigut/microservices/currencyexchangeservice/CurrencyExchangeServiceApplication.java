e4520748f9b3a4b4fedf1fc8319dc63e7d5b89b6
Chris Migut <cmigut@mail.usf.edu>
2017-11-27 03:52:58 -0500

    Registered the currency-exchange services to Eureka naming server

--- practice-service2service-jpa-without-tests/currency-exchange-service/src/main/java/com/cmigut/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ practice-service2service-jpa-without-tests/currency-exchange-service/src/main/java/com/cmigut/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 	
 	@Bean
