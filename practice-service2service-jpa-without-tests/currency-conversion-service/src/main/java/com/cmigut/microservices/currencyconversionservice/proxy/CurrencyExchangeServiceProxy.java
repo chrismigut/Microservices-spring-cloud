a5442c5d8b069ac959670b9708277dd3832ae0d2
Chris Migut <cmigut@mail.usf.edu>
2017-11-26 22:34:01 -0500

    Added Feign proxy to currency-conversion, will remove the RestTemplate in next commit

---
+++ practice-service2service-jpa-without-tests/currency-conversion-service/src/main/java/com/cmigut/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,17 @@
+package com.cmigut.microservices.currencyconversionservice.proxy;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cmigut.microservices.currencyconversionservice.bean.CurrencyConversion;
+
+
+//name is the service we are going to call
+@FeignClient(name="currency-exchange-service", url="localhost:8000")
+public interface CurrencyExchangeServiceProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(@PathVariable String from,@PathVariable String to);
+
+}
